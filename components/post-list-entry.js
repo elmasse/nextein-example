31213f8379fe5683af0f61f02c447948d8223378
Max Fierro <elmasse@gmail.com>
2017-06-25 15:08:50 -0300

    show date

--- components/post-list-entry.js
+++ components/post-list-entry.js
@@ -4,9 +4,12 @@ import React from 'react'
 import { Content } from 'nextein/post'
 
 export default ({ data, content }) => {
-  return ( 
+  const { url, title, date } = data
+
+  return (
     <article>
-      <h1><a href={data.url}>{data.title}</a></h1>
+      <h1><a href={url}>{title}</a></h1>
+      <span>{`${new Date(date).toDateString()}`}</span>
       <Content data={data} content={content} excerpt/>
     </article>
   )
