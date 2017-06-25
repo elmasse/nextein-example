665f665b187ef9219cbb7e3f1a9971333b7c8c70
Max Fierro <elmasse@gmail.com>
2017-06-25 15:33:32 -0300

    post styles, navigation. WIP

---
+++ components/navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default ({ style }) => {
+  return (
+    <nav style={style}>
+      <a href="/">Home</a>
+    </nav>
+  )
+}
+
+const styles = {
+  nav: {
+    display: 'flex'
+  },
+  navItem: {
+
+  }
+
+}
--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,8 @@
 
 import React, { Component } from 'react'
-
 import withPost, { Content } from 'nextein/post'
 
+import Navigation from '../components/navigation'
 
 class Post extends Component {
 
@@ -10,12 +10,31 @@ class Post extends Component {
     const { post } = this.props
     const { data, content } = post
     return (
-      <section>
-        <h1>{data.title}</h1>
-        <Content {...post}/>
-      </section>
+      <main style={styles.main}>
+        <Navigation style={styles.section}/>
+        <article style={styles.section}>
+          <h1>{data.title}</h1>
+          <Content {...post}/>
+        </article>
+      </main>
     )
   }
 }
 
 export default withPost(Post)
+
+const styles = {
+  main: {    
+    fontFamily: '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
+    fontWeight: 100,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '60vw' 
+  }
+
+}
