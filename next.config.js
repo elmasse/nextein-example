126c3e3325ecadcb5eb14b60c7eeefc504fcbd58
Max Fierro <elmasse@gmail.com>
2017-10-02 15:57:13 -0300

    update examples for beta22

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
 
-const nexteinConfig = require('nextein/config').default
+const { default: config } = require('nextein/config')
+const { entries, inCategory } = require('nextein/posts')
 
-const stories = ['food', 'cars'].reduce( (prev, entry) => ({
-    ...prev,
-    [`/${entry}`]: { page: '/stories', query: { category: `stories/${entry}` } }
-}), {})
+const getStories = async () =>  {
+  const category = 'stories'
+  const all = await entries()
+  return all
+    .filter(inCategory(category, {includeSubCategories: true}))
+    .map(post => post.data.category)
+    .filter((category, idx, arr) => arr.indexOf(category) === idx )
+    .reduce((prev, entry) => ({
+      ...prev,
+      [`/${entry.replace(`${category}/`, '')}`]: { page: '/stories', query: { category: `${entry}` } }
+    }), {})
+}
 
-module.exports = nexteinConfig({
-    exportPathMap: () => ({
-        '/all-posts': { page: '/all-posts' },
-        '/sub-section': { page: '/sub-section' },
-        '/tags': { page: '/tags'},
-        ...stories
+
+module.exports = config({
+  exportPathMap: async () => {
+      const stories = await getStories()
+      console.log(stories)
+      return ({
+      '/all-posts': { page: '/all-posts' },
+      '/sub-section': { page: '/sub-section' },
+      '/tags': { page: '/tags' },
+      ...stories
     })
+  }
 })
+
