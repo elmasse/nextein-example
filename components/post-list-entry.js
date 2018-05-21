7b9a5cdb19f664ec60d7eb76ac023f9d786f93f5
Max Fierro <elmasse@gmail.com>
2018-05-21 19:05:22 -0300

    Count words example.

--- components/post-list-entry.js
+++ components/post-list-entry.js
@@ -3,16 +3,27 @@ import React from 'react'
 import Link from 'nextein/link'
 import { Content } from 'nextein/post'
 
-const PostListEntry = ({ data, content, excerpt=true }) => {
-  const { url, title, date, _entry, page = 'post' } = data
+const countWords = (s) => {
+  s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
+  s = s.replace(/[ ]{2,}/gi," ");//2 or more space to 1
+  s = s.replace(/\n /,"\n"); // exclude newline with a start spacing
+  return s.split(' ').length; 
+}
 
+const readingTime = (words) => {
+  return Math.ceil(words/200)
+}
+
+const PostListEntry = ({ data, content, raw, excerpt=true }) => {
+  const { url, title, date, _entry, page = 'post' } = data
+  const words = countWords(raw)
+  const readTime = readingTime(words)
   return (
     <article>
       <h1>
-        {/* <a href={url}>{title}</a> */}
-        <Link data={data} content={content}><a>{title}</a></Link>
+        <Link data={data}><a>{title}</a></Link>
       </h1>
-      <span>{`${new Date(date).toDateString()}`}</span>
+      <span>{`${new Date(date).toDateString()} | ~ ${readTime} mins | ${words} words`}</span>
       <Content data={data} content={content} excerpt={excerpt}/>
     </article>
   )
